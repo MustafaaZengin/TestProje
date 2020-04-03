3fd5e09adda5b2fae6ac1bfdd5de7067d73ae66b
MustafaaZengin <mz.mustafa.zengin@gmail.com>
2020-04-03 11:42:13 +0300

    sonardeneme

--- src/test/java/com/n11/test/setting.java
+++ src/test/java/com/n11/test/setting.java
@@ -2,21 +2,27 @@ package com.n11.test;
 
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
+import org.omg.CORBA.TIMEOUT;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class setting {
     protected static WebDriver driver;
+    static WebDriverWait wait;
     static String webSiteMainUrl="http://www.n11.com";
 
     @BeforeClass
     public static void setUp(){
-        System.setProperty("webdriver.chrome.driver","C:\\SeleniumDriver\\chromedriver1.exe");
+        System.setProperty("webdriver.chrome.driver","driver/chromedriver.exe");
          driver=new ChromeDriver();
     }
     @AfterClass
     public static void after() throws InterruptedException{
-        Thread.sleep(3000);
+        System.out.println("mustafa");
+        // Thread.sleep(3000);
     }
     public  String getUrl(){
         return webSiteMainUrl;
